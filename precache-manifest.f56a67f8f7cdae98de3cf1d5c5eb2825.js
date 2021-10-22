41f04b4bbc5a8c0589f7c095200f6df1388ed1ea
trongphuc3105 <91412382+Rememberlibrary@users.noreply.github.com>
2021-10-22 20:14:33 +0700

    Updates

---
+++ precache-manifest.f56a67f8f7cdae98de3cf1d5c5eb2825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7f6562dd7a2c1a2ae3a0eacd93a368",
+    "url": "/messages/index.html"
+  },
+  {
+    "revision": "7926729d608e18226174",
+    "url": "/messages/static/css/main.cbc5f8ba.chunk.css"
+  },
+  {
+    "revision": "71f5cba660af878f7355",
+    "url": "/messages/static/js/2.db454553.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/messages/static/js/2.db454553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7926729d608e18226174",
+    "url": "/messages/static/js/main.bbcfeb7d.chunk.js"
+  },
+  {
+    "revision": "7d0dde2d50a119a28f3f",
+    "url": "/messages/static/js/runtime-main.b8044ac7.js"
+  }
+]);
