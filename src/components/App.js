5922e4fd7cf61337f96794803d6e32202097c715
Huỳnh Trọng Phúc <91412382+Rememberlibrary@users.noreply.github.com>
2021-10-22 21:32:08 +0700

    asdd

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
                     <AuthProvider>
                          <Switch>
                               <Route path="/chats" component={Chats} />
-                              <Route path="/" component={Login} />
+                              <Route path="/messages" component={Login} />
                          </Switch>
                     </AuthProvider>
                </Router>
